67e07742e4795252da52f2d970c5cabb4058947a
kramstyles <kramstyles@outlook.com>
2022-08-13 15:57:01 +0100

    i give up

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ class Show(db.Model):
     __tablename__ = 'Shows'
 
     id = db.Column(db.Integer, primary_key=True)
-    venue = db.relationship('Venue', backref='venue', lazy=True)
-    artist = db.relationship('Artist', backref='artist', lazy=True)
+    venue_id = db.relationship('Venue', backref='venue', lazy=True)
+    artist_id = db.relationship('Artist', backref='artist', lazy=True)
     start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
@@ -521,15 +521,19 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-    # called to create new shows in the db, upon submitting new show listing form
-    # TODO: insert form data as a new Show record in the db, instead
-
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-    return render_template('pages/home.html')
+    try:
+        info = request.form
+        data = Show(**info)
+        db.session.add(data)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
+    except Exception as err:
+        db.session.rollback()
+        flash('An error occurred. Show could not be listed. Reason: ' + str(err))
+    finally:
+        db.session.close()
+        return render_template('pages/home.html')
 
 
 @app.errorhandler(404)
